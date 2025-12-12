1192f4d19a8fa0c0f62e1ac0ccd9dd10761c5de1
Abhi Markan <abhi.markan@justice.gov.uk>
2025-12-12 17:41:00 +0000

    feat(22-gitleaks): added redact argument

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,1 +1,7 @@
+# Install Git version control system using Alpine Package Keeper (apk)
+# This command adds the git package without caching the package index locally,
+# which helps reduce the Docker image size when used in containers.
+# The --no-cache flag prevents apk from storing downloaded package files.
+#
+
 apk add --no-cache git
