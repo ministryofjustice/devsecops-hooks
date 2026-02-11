e7ab75cd5c2df24e0cd19b308d8b2017e17d7862
Abhi Markan <abhi.markan@justice.gov.uk>
2026-02-11 17:24:23 +0000

    feat(133-lint): fixed bash script

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -38,23 +38,21 @@ fi
 cd /src || { echo "❌ Unable to find /src directory."; exit 1; }
 
 # Argument build
+CONFIGURATION_ARGUMENT=()
 if [ -f "$GITLEAKS_CONFIGURATION_FILE" ]; then
-    CONFIGURATION_ARGUMENT="--config $GITLEAKS_CONFIGURATION_FILE"
-else
-    CONFIGURATION_ARGUMENT=""
+    CONFIGURATION_ARGUMENT=(--config "$GITLEAKS_CONFIGURATION_FILE")
 fi
 
+IGNORE_ARGUMENT=()
 if [ -f "$GITLEAKS_IGNORE_FILE" ]; then
-    IGNORE_ARGUMENT="--gitleaks-ignore-path $GITLEAKS_IGNORE_FILE"
-else
-    IGNORE_ARGUMENT=""
+    IGNORE_ARGUMENT=(--gitleaks-ignore-path "$GITLEAKS_IGNORE_FILE")
 fi
 
 # GitLeaks
 if [ "$GIT_MODE" != "false" ]; then
-    gitleaks git --pre-commit --redact --staged --verbose --exit-code 1 "$CONFIGURATION_ARGUMENT" "$IGNORE_ARGUMENT"
+    gitleaks git --pre-commit --redact --staged --verbose --exit-code 1 "${CONFIGURATION_ARGUMENT[@]}" "${IGNORE_ARGUMENT[@]}"
 else
-    gitleaks detect --source . --no-git --redact --report-format json --exit-code 1 --verbose "$CONFIGURATION_ARGUMENT" "$IGNORE_ARGUMENT"
+    gitleaks detect --source . --no-git --redact --report-format json --exit-code 1 --verbose "${CONFIGURATION_ARGUMENT[@]}" "${IGNORE_ARGUMENT[@]}"
 fi
 
 # Successful
